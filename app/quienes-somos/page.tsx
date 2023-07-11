eaaf1b787614e6b5973587a58696652e27a8b335
isai <isaiorellana.dev@gmail.com>
2023-07-10 21:13:24 -0600

    💄 Rutas principales, header y primeros estilos

---
+++ app/quienes-somos/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutUs() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h2>Sobre nosotros</h2>
+    </main>
+  )
+}
