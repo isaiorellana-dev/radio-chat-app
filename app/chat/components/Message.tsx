21d0bb2d160bc5a5952952cd7e42dab14a6b68ce
isai <isaiorellana.dev@gmail.com>
2023-07-12 15:59:29 -0600

    💄 Estilos en el chat, incompletos

--- app/chat/components/Message.tsx
+++ app/chat/components/Message.tsx
@@ -1,12 +1,14 @@
 import { message as propsInt } from "../../../interfaces/res"
 
-export default function Message(props: { message: propsInt }) {
-  const { message } = props
+export default function Message(props: { message: propsInt; index: number }) {
+  const { message, index } = props
+
+  const esPar = (n: number): boolean => n % 2 === 0
 
   return (
-    <div>
-      <p className="text-left">
-        <span className="font-semibold">{message.nickname}: </span>
+    <div className={esPar(index) ? "bg-slate-700" : "bg-slate-800"}>
+      <p className="text-left text-cyan-100 max-w-sm px-0.5">
+        <span className="font-semibold text-cyan-50">{message.nickname}: </span>
         {message.body}
       </p>
     </div>
--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,14 +1,20 @@
 "use client"
 import { getMessages } from "@/utils/messages"
 import Message from "./components/Message"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { message } from "@/interfaces/res"
 
 export default function Chat() {
   const [messages, setMessages] = useState<Array<message>>([])
+  const mainRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
     getMessages().then((res) => setMessages(res))
+
+    if (mainRef.current) {
+      console.log("hello")
+      mainRef.current.scrollTop = mainRef.current.clientHeight
+    }
 
     const socket = new WebSocket("ws://localhost:5050/ws")
 
@@ -29,10 +35,17 @@ export default function Chat() {
   }, [])
 
   return (
-    <main className="flex min-h-screen flex-col items-center py-24 px-3">
+    <main
+      className="flex flex-col items-center"
+      style={{
+        height: "calc(100vh - 80px)",
+        overflowY: "scroll",
+      }}
+      ref={mainRef}
+    >
       <div>
-        {messages.map((m) => (
-          <Message key={m.id} message={m} />
+        {messages.map((m, i) => (
+          <Message key={m.id} message={m} index={i} />
         ))}
       </div>
     </main>
